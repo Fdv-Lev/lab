470795c99cf6995df5987a5011a4e2372a904146
Fdv-Lev <lfadeev500@gmail.com>
2024-10-18 18:27:46 +0500

    111111.js

--- 11/111111.js
+++ 11/111111.js
@@ -1,22 +1,12 @@
-//const buttons = document.querySelectorAll('.btn');
 
-//buttons.forEach(button => {
-    //button.addEventListener('click', () => {
-        // Генерируем случайный цвет в формате HEX
-        //const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        //button.style.backgroundColor = randomColor;
-   // });
-//});
 const buttons = document.querySelectorAll('.btn');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        // Генерируем случайный цвет в формате HEX
         const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
 
-        // Изменяем цвет и форму кнопки
         button.style.backgroundColor = randomColor;
-        button.style.transform = 'scale(0.9)'; // Уменьшаем размер кнопки при клике
+        button.style.transform = 'scale(0.9)';
         button.style.transform = 'scale(0.9) rotate(360deg)';
         button.style.transition = 'all 0.3s ease-in-out';
     });
